83d32698025a1fff6bef9d1fe508cd4d548df33c
Git Try <xcy010501@gmail.com>
2022-10-13 20:37:03 -0700

    This is for the future's clone

--- OpenGL/OpenGL/Source.cpp
+++ OpenGL/OpenGL/Source.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -7,6 +8,8 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    glewInit();
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
